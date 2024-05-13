793ab0354103d79c4caefe070b166c8f53b51227
Oladosu Feranmi <oladosuferanmi@gmail.com>
2024-05-13 10:49:14 +0100

    majr build

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "MVM-Hyper Count-0.1.0";
+const cacheName = "MVM-Hyper Count-0.1.2";
 const contentToCache = [
     "Build/Builds.loader.js",
     "Build/Builds.framework.js.unityweb",
